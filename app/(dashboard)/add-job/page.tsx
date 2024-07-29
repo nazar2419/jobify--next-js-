a214ed91428cbb51179fa8e81c479a5f7ed56c61
Nazar <nazarx095@gmail.com>
2024-07-29 15:11:37 +0300

    form component

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,6 +1,8 @@
+import CreateJobForm from "@/components/CreateJobForm"
+
 function  AddJobPage() {
   return (
-    <h1 className="text-4xl">AddJobPage</h1>
+    <CreateJobForm />
   )
 }
 export default AddJobPage
---
+++ components/CreateJobForm.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+    JobStatus,
+    JobMode,
+    createdAndEditJobSchema,
+    createdAndEditJobType,
+} from '@/utils/types';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+
+import { CustomFormField, CustomFormSelect } from "./FormComponents";
+
+
+function CreateJobForm() {
+    // 1.Define your form.
+    const form = useForm<createdAndEditJobType>({
+        resolver: zodResolver(createdAndEditJobSchema),
+        defaultValues: {
+            position: '',
+            company: '',
+            location: '',
+            status: JobStatus.Pending,
+            mode: JobMode.FullTime,
+        },
+    });
+
+    // 2. Define a submit handler.
+    function onSubmit(values: createdAndEditJobType) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    }
+  return (
+    <Form {...form}>
+       <form onSubmit={form.handleSubmit(onSubmit)} className="bg-muted p-8 rounded">
+            <h2 className="capitalize font-semibold text-4xl mb-6">
+                 add job
+            </h2>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
+                {/* posion */}
+                <CustomFormField name="position" control={form.control} />
+								{/* company */}
+								<CustomFormField name="company" control={form.control} />
+								{/* posion */}
+								<CustomFormField name="location" control={form.control} />
+								{/* job status */}
+								<CustomFormSelect 
+									name="status"
+									control={form.control}
+									labelText="job status"
+									items={Object.values(JobStatus)}
+								/>
+								{/* job type */}
+								<CustomFormSelect 
+									name="mode"
+									control={form.control}
+									labelText="job mode"
+									items={Object.values(JobMode)}
+								/>
+							  <Button type="submit" className="self-end capitalize">
+									Submit
+								</Button>
+            </div>
+        </form>     
+    </Form>
+  )
+}
+export default CreateJobForm
